9e3c33118c99ccd5d96cd09e9726ec0be13e41da
ArtGameCode <43046551+ArtGameCode@users.noreply.github.com>
2018-10-14 01:34:55 -0500

    Added search.php.

---
+++ Sem_Proj/search.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("Template.php");
+
+$page = new Template("Home");
+$page->setHeadSection("<script src='assign1js.js'></script><link rel='stylesheet' href='assign1style.css'>");
+$page->setTopSection();
+$page->setBottomSection();
+
+print $page->getTopSection();
+
+print " <nav>\n";
+print "  <ul>\n";
+print "   <li><a href='home.php'>Home</a></li>\n";
+print "   <li><a href='about.php'>About</a></li>\n";
+print "   <li><a href='contact.php'>Contact Us</a></li>\n";
+print "   <li><a href='#' class='active'>Search</a><li>\n";
+print "  </ul>\n";
+print " </nav>\n";
+
+print " <div class='main'>\n";
+print "     <h1>Search page </h1>\n";
+print "     <form>\n";
+print "         <input type='text' name='search' placeholder='Search..'>\n";
+print "     </form>\n";
+print " </div>\n";
+
+print $page->getBottomSection();
+
+?>
