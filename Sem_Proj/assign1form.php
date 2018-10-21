9c1abed4161db0192430286e4fa4457e1c1d7701
nimmer47 <44216316+nimmer47@users.noreply.github.com>
2018-10-21 12:58:44 -0500

    Adding Thank You for contact page.
    Added code to the thank you for contacting us page to enter the contact request into the database.

--- Sem_Proj/assign1form.php
+++ Sem_Proj/assign1form.php
@@ -1,13 +1,11 @@
 <?php
 require_once("Template.php");
-
-$page = new Template("Script");
+require_once("DB.class.php");
+$page = new Template("Thank You");
 $page->setHeadSection("<script src='assign1form.php'></script><link rel='stylesheet' href='assign1style.css'>");
 $page->setTopSection();
 $page->setBottomSection();
-
 print $page->getTopSection();
-
 print "<nav>\n";
 print " <ul>\n";
 print "  <li><a href='home.php'>Home</a></li>\n";
@@ -16,19 +14,45 @@ print "  <li><a href='#' class='active'>Contact Us</a></li>\n";
 print "  <li><a href='search.php'>Search</a><li>\n";
 print " </ul>\n";
 print "</nav>\n";
-
 function checkEmpty($value) {
 	return !empty($value);
 }
-
 if (isset($_POST['email']) && checkEmpty($_POST['email']) && isset($_POST['comment']) && checkEmpty($_POST['comment'])) {
 	print "Thank you for contacting us, someone will get back to you shortly";
 }else{
 	echo "Error"; 
 }
-//var_dump($_POST);
+
+//Create DB object
+$db = new DB();
+
+//Test connection
+if (!$db->getConnStatus()) {
+  print "An error has occurred with connection\n";
+  exit;
+}
+
+//Set terms from POST
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$comment = $_POST["comment"];
+
+//Sanitize Input Term For Database
+$emailSan = $db->dbEsc($email);
+$phoneSan = $db->dbEsc($phone);
+$commentSan = $db->dbEsc($comment);
+
+//Input Query String
+$query = "INSERT INTO contactRequests (inserttime,email,phone,message) VALUES (now(), $emailSan, $phoneSan, $commentSan);";
+
+//Execute Query
+$result = $db->dbCall($query);
+
+//Print error if failed query
+if(!$result){
+	print "Error \n";
+	exit;
+}
 
 print $page->getBottomSection();
-
-
 ?>
