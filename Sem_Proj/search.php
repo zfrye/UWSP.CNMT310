1710cdf1fc3a068b10189a75fe3a72232d79581b
nimmer47 <44216316+nimmer47@users.noreply.github.com>
2018-10-21 12:32:34 -0500

    Add files via upload
    Adding name and action to search page form.

--- Sem_Proj/search.php
+++ Sem_Proj/search.php
@@ -1,14 +1,10 @@
 <?php
-
 require_once("Template.php");
-
 $page = new Template("Home");
 $page->setHeadSection("<script src='assign1js.js'></script><link rel='stylesheet' href='assign1style.css'>");
 $page->setTopSection();
 $page->setBottomSection();
-
 print $page->getTopSection();
-
 print " <nav>\n";
 print "  <ul>\n";
 print "   <li><a href='home.php'>Home</a></li>\n";
@@ -17,14 +13,11 @@ print "   <li><a href='contact.php'>Contact Us</a></li>\n";
 print "   <li><a href='#' class='active'>Search</a><li>\n";
 print "  </ul>\n";
 print " </nav>\n";
-
 print " <div class='main'>\n";
 print "     <h1>Search page </h1>\n";
-print "     <form>\n";
+print "     <form name='searchForm' action='./results.php' method='Post'>\n";
 print "         <input type='text' name='search' placeholder='Search..'>\n";
 print "     </form>\n";
 print " </div>\n";
-
 print $page->getBottomSection();
-
 ?>
