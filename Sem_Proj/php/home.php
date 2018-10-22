529e79b229596ee8e66f25203e3e64d54a0d235f
dlama428 <43046551+dlama428@users.noreply.github.com>
2018-10-22 11:09:12 -0500

    setting up file structure

---
+++ Sem_Proj/php/home.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("../class/Template.php");
+require_once("../class/NavBar.php");
+
+$page = new Template("Home");
+$page->setHeadSection("<link rel='stylesheet' href='../css/semStyle.css'>");
+$page->setTopSection();
+$page->setBottomSection();
+
+print $page->getTopSection();
+
+$nav = new NavBar(basename($_SERVER['REQUEST_URI']));
+$nav->setNavSection();
+print $nav->getNav();
+
+print " <div class='main'>\n";
+print " 	<h1>Home</h1>\n";
+print " 	<p>This is the homepage for Sprint 1 Assignment.</p>\n";
+print "	</div>\n";
+
+print $page->getBottomSection();
+
+?>
