a2cc92cba74f282f225af72d62893c44b6a48a47
qianyangL <43143655+qianyangL@users.noreply.github.com>
2018-11-13 10:48:10 -0600

    Add files via upload
    I changed the position of the text area. that's it.

--- Sem_Proj/sprint2/php/contact.php
+++ Sem_Proj/sprint2/php/contact.php
@@ -1,10 +1,7 @@
 <?php
 
-session_start();
-
 require_once("../class/Template.php");
 require_once("../class/NavBar.php");
-require_once("../class/ModalLogin.php");
 
 $page = new Template("Contact");
 $page->setHeadSection("<link rel='stylesheet' href='../css/semStyle.css'>");
@@ -14,22 +11,8 @@ $page->setBottomSection();
 print $page->getTopSection();
 
 $nav = new NavBar(basename($_SERVER['REQUEST_URI']));
-if(isset($_SESSION['isLogged'])){
-	$nav->setLog($_SESSION['isLogged']);
-}
 $nav->setNavSection();
-$log = new Login();
-$log->setLogin();
 print $nav->getNav();
-if(isset($_SESSION['isAdmin'])){
-	$nav->setAdmin($_SESSION['isAdmin']);
-	if($_SESSION['isAdmin'] == true){
-		$nav->setAdminNav;
-		print $nav->getAdminNav;
-	}
-}
-print $log->getLogin();
-
 
 print "<div class='main'>\n";
 print " <h1>Contact Us</h1>\n";
@@ -37,10 +20,11 @@ print "   <form action='./actionThankyou.php' method='post' id='contactform'>\n"
 print "     Email Address: <input type='email' name='email'><br/>\n";
 print "     Phone Number: <input type='number' name='phone'><br/>\n";
 print "     <br/>\n";
+print "<textarea name='comment' form='contactform' placeholder='Enter text here...'></textarea>\n";
+print "     <br/>\n";
 print "     <input type='submit'> <br/>\n";
 print "     <br/>\n";
 print "   </form>\n";
-print "   <textarea name='comment' form='contactform' placeholder='Enter text here...'></textarea>\n";
 print "</div>\n";
 
 print $page->getBottomSection();
