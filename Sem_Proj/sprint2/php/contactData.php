29a1bbe9bd30fb1b2b7b44d768eb358020e4e742
Danielle LaMarche <dlama391@uwsp.edu>
2018-11-15 20:11:57 -0600

    Adding error message on contactData.php page

--- Sem_Proj/sprint2/php/contactData.php
+++ Sem_Proj/sprint2/php/contactData.php
@@ -31,48 +31,67 @@ print $nav->getNav();
 print $nav->getAdminNav();
 print $log->getLogin();
 
-print " <div class='main'>\n";
-print " 	<h1>Contact Data Page</h1>\n";
-
-//Create DB object
-$db = new DB();
-
-//Test connection
-if (!$db->getConnStatus()) {
-	print "An error has occurred with connection\n";
-	exit;
-}
-
-//Search Query String
-$query = "SELECT * FROM contactrequests";
-
-//Execute Query
-$result = $db->dbCall($query);
-
-//Print error if failed query
-if(!$result){
-	print "No Results Found \n";
-	exit;
-}
-
-//Print out Results
-print "			<table style='width:100%'> \n";
-print"				<tr>\n";
-print"				<th>ID</th>\n";
-print"				<th>Date Submitted</th>\n";
-print"				<th>Email</th>\n";
-print"				<th>Phone Number</th>\n";
-print"				<th>Message</th>\n";
-print"				</tr>\n";
-foreach($result as $row){
-	print"				<tr>\n";
-	foreach($row as $item){
-		print"					<td>$item</td>\n";
+if(isset($_SESSION['isLoggedIn'])){
+	$nav->setLog($_SESSION['isLoggedIn']);
+	if(isset($_SESSION['isAdmin'])){
+		$nav->setAdmin($_SESSION['isAdmin']);
+		if($_SESSION['isAdmin'] == true){
+			$nav->setAdminNav();
+		}
 	}
-	print"				</tr>\n";
 }
 
-print "	</div>\n";
+if (isset($_SESSION['isAdmin'])) {
+	
+	print " <div class='main'>\n";
+	print " 	<h1>Contact Data Page</h1>\n";
+
+	//Create DB object
+	$db = new DB();
+
+	//Test connection
+	if (!$db->getConnStatus()) {
+		print "An error has occurred with connection\n";
+		exit;
+	}
+
+	//Search Query String
+	$query = "SELECT * FROM contactrequests";
+
+	//Execute Query
+	$result = $db->dbCall($query);
+
+	//Print error if failed query
+	if(!$result){
+		print "No Results Found \n";
+		exit;
+	}
+
+	//Print out Results
+	print "			<table style='width:100%'> \n";
+	print"				<tr>\n";
+	print"				<th>ID</th>\n";
+	print"				<th>Date Submitted</th>\n";
+	print"				<th>Email</th>\n";
+	print"				<th>Phone Number</th>\n";
+	print"				<th>Message</th>\n";
+	print"				</tr>\n";
+	foreach($result as $row){
+		print"				<tr>\n";
+		foreach($row as $item){
+			print"					<td>$item</td>\n";
+		}
+		print"				</tr>\n";
+	}
+
+	print "	</div>\n";
+
+}else{
+	print " <div class='main'>\n";
+	print " 	<h2>Error</h2>\n";
+	print "		<p>You don't have permission to access this page.</p>\n";
+	print "	</div>\n";
+}
 
 print "<script src='../js/myLogin.js'></script>";
 print $page->getBottomSection();
