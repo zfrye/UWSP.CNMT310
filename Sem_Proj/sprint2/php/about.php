918e29a465ce0e4d1f002f626468213389844639
ArtGameCode <43046551+ArtGameCode@users.noreply.github.com>
2018-11-16 18:00:59 -0600

    adding to about.php and formatting h2

--- Sem_Proj/sprint2/php/about.php
+++ Sem_Proj/sprint2/php/about.php
@@ -34,61 +34,58 @@ print $log->getLogin();
 
 print "<div class='main'>\n";
 print "<h1>About Us</h1>\n";
-print "<p>This is the about page for Sprint 1 Assignment, due on 10/23/2018 at 11:59pm Central Time. <br/><br/>
-       This is a group assignment. You are expected to work within your group to complete this assignment.
-       Do not work with or collaborate with other groups.
-       Use the Template class to create a website. The website should consist of the following pages....<br/>\n";
-print "  <br/>\n";
+print "<h2>Sprint 2</p></h2>\n";
+print "<p>Due 11/18/2018 - 11:59pm Central Time</p>
+       <p>This is a group assignment. You are expected to work within your group to complete this assignment. Do not work with or collaborate with other groups.\n"; 
+print " <b><p>The website should:</p></b>\n";
 print "  <ul>\n";
-print "      <li><p>Choose a web site from the individual assignment (assignment 1) for your group to use for this
-                assignment. Ensure that the menu links to all pages (except the 'Thank you' page, which is
-                reachable through the Contact form). Correct any deficiencies noted within the existing code:
-                Be sure to use the original Assignment 1 to look for deficiencies, even if those were not noted in
-                the grading.</p></li>\n";
-print "      <li><p>Create a new page, linked within the menu, that enables the user to search for a book. The
-                search should use the provided Database class to query the database across the title, author,
-                and isbn fields using a single query. In other words, the web form should not require that the
-                user chooses the ISBN, author, or title. The form pages should be styled to look like the other
-                pages on the site.</p></li>\n";
-print "      <li><p>Create a result page that displays the matching results from the database in an HTML table,
-                again with the same layout (look and feel) as the other pages.</p></li>\n";
+print "      <li><p>Implement the authentication database schema discussed in lecture. You should manually create at least two users, one a “normal” user and one an “administrator” user.</p></li>\n";
+print "      <li><p>Create a web form that enables a user to authenticate using credentials from the authentication database implemented as part of this assignment. Based on our discussion in lecture, the form should use the correct input types for this type of page and display an appropriate error message if authentication fails.</p></li>\n";
+print "      <li><p>On successful authentication, display (in the header/menu area of each page that the user then clicks on) a message indicating that the user is logged in, like “Welcome Steve”. Add a link called “Log Out” that logs the user out such that page no longer recognize them as being authenticated and redirects them back to the home page after doing so. Be sure the Log Out link does not appear when not logged in.</p></li>\n";
+print "      <li><p>Create a page called Contact Data that contains the information from the contact form database in an HTML table. The page should only be viewable to users with Administrator privileges and should display an error for unauthenticated users or users without the Administrator role.</p></li>\n";
+print "      <li><p>Display a menu item for the Contact Data page only if the user is an Administrator.</p></li>\n";
+print "      <li><p>Note that because this assignment builds on the previous sprint assignment, you should consider adding items to the backlog that were recommended or discussed as part of the feedback for that assignment.</p></li>\n";
+print "  </ul>\n";
+  
+print "  <b><p>Deliverable:</p></b>\n";
+print "  <ul><p>A PDF or txt file in the dropbox containing git commit ids for all group members along with narrative for testing. You must also include the URL for testing the site and credentials for both types of users implemented as part of this assignment. Missing (or broken) URL or missing credentials will result in a grade reduction.</p></ul><br/>\n";
+                
+
+
+
+print "<h2>Sprint 1</h2>\n";
+print "<p>Due 10/23/2018 - 11:59pm Central Time</p>
+       <p>This is a group assignment. You are expected to work within your group to complete this assignment.
+       Do not work with or collaborate with other groups. Use the Template class to create a website.\n"; 
+print " <b><p>The website should consist of the following pages:</p></b>\n";
+print "  <ul>\n";
+print "      <li><p>Choose a web site from the individual assignment (assignment 1) for your group to use for this assignment. Ensure that the menu links to all pages (except the 'Thank you' page, which is reachable through the Contact form). Correct any deficiencies noted within the existing code: Be sure to use the original Assignment 1 to look for deficiencies, even if those were not noted in the grading.</p></li>\n";
+print "      <li><p>Create a new page, linked within the menu, that enables the user to search for a book. The search should use the provided Database class to query the database across the title, author, and isbn fields using a single query. In other words, the web form should not require that the user chooses the ISBN, author, or title. The form pages should be styled to look like the other pages on the site.</p></li>\n";
+print "      <li><p>Create a result page that displays the matching results from the database in an HTML table, again with the same layout (look and feel) as the other pages.</p></li>\n";
 print "      <li><p>Change the Contact form to place all captured information into a database.</p></li>\n";
 print "    </ul>\n";
-print "  <br/>\n";
-  
 print "  <b><p>Notes:</p></b>\n";
 print "    <ul>\n";
-print "      <li><p>All code must produce valid HTML5 and valid CSS must be used. All code will be tested on
-                cnmtsrv2 and therefore should assume running on Linux. A portion of the grade includes code
-                styling and the use of minimal code along with an optimal algorithm for the problem being
-                solved.</p></li>\n";
-print "      <li><p>All work should be shared among team members and code should be created in a directory
-                called sprint1, with subdirectories for css, js, classes, and so on.</p></li>\n";
+print "      <li><p>All code must produce valid HTML5 and valid CSS must be used. All code will be tested on cnmtsrv2 and therefore should assume running on Linux. A portion of the grade includes code styling and the use of minimal code along with an optimal algorithm for the problem being solved.</p></li>\n";
+print "      <li><p>All work should be shared among team members and code should be created in a directory called sprint1, with subdirectories for css, js, classes, and so on.</p></li>\n";
 print "    </ul>\n";
-print "    <br/>\n";
-
 print "  <b><p>Deliverable:</p></b>\n";
-print "  <ul><p>A PDF or plaintext document in the Dropbox containing the commit id of each team member along with
-            any narrative to explain how to view and/or test the code.</p></ul><br/>\n";
-  
+print "  <ul><p>A PDF or plaintext document in the Dropbox containing the commit id of each team member along with any narrative to explain how to view and/or test the code.</p></ul>\n";
 print "  <b><p>Hints:</p></b>\n";
 print "  <ul>\n";
-print "    <li><p>Coordinate on a solution - work together to determine how best to accomplish all tasks - avoid
-                overlapping or duplicating work.</p></li>\n";
+print "    <li><p>Coordinate on a solution - work together to determine how best to accomplish all tasks - avoid overlapping or duplicating work.</p></li>\n";
 print "    <li><p>Much effort will be spent around writing the query to search the bookinfo database.</p></li>\n";
 print "    <li><p>Remember to escape form input before using it in a database query/insert.</p></li>\n";
-print "    <li><p>There is opportunity to divide work so that team members can utilize their strengths. For
-                example, if one team member has experience working with databases, have that person work
-                on the database portion(s) of the assignment. If another team member has CSS experience,
-                have that person work on the layout.</p></li>\n";
+print "    <li><p>There is opportunity to divide work so that team members can utilize their strengths. For example, if one team member has experience working with databases, have that person work on the database portion(s) of the assignment. If another team member has CSS experience, have that person work on the layout.</p></li>\n";
 print "    <li><p>Work early and do not underestimate the amount of work involved.</p></li>\n";
 print "  </ul>\n";
 print "  <br/>\n";
 
-
 print "  <p>Created by CNMT 310 - Group 2.</p>\n";
-print "</p>\n";
 print "</div>\n";
+
+
+
 print "<script src='../js/myLogin.js'></script>";
 print "<script src='../js/myLogin.js'></script>";
 print $page->getBottomSection();
