1af538606cabb7a151926430cf64715e444736f5
Chandler Nimmer <Cnimm823@uwsp.edu>
2018-11-28 10:44:44 -0600

    Removing template class from logout page.

--- Sem_Proj/sprint3/php/logout.php
+++ Sem_Proj/sprint3/php/logout.php
@@ -12,20 +12,7 @@ unset($_SESSION['Name']);
 unset($_SESSION['Email']);
 session_destroy();
 
-require_once("../class/Template.php");
-require_once("../class/NavBar.php");
-require_once("../class/ModalLogin.php");
-
-$page = new Template("Home");
-$page->setHeadSection("<link rel='stylesheet' href='../css/semStyle.css'>");
-$page->setTopSection();
-$page->setBottomSection();
-
-print $page->getTopSection();
-
 header('Location: ../php/home.php');
 exit;
-
-print $page->getBottomSection();
 
 ?>
